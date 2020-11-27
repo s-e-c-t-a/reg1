161e8d5879ddcb80227b5d3ed8e01b644e7918a3
q <1070711@gmail.com>
2020-11-27 03:19:46 +0300

    показывает цифру

--- lib/reg1_web/views/layout_view.ex
+++ lib/reg1_web/views/layout_view.ex
@@ -1,3 +1,31 @@
 defmodule Reg1Web.LayoutView do
   use Reg1Web, :view
+
+  import Ecto.Query, warn: false
+  
+  alias Reg1.Repo
+  alias Reg1.Users.Score
+
+  def title(conn) do
+    find_id = conn.assigns.current_user.id
+
+      query =
+      from s in Score,
+      where: s.user_id == ^find_id 
+      [pre_wallet] = Repo.all(query)
+
+      find_wallet = pre_wallet.wallet
+  IO.puts "_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_"
+    IO.inspect(find_wallet)
+    IO.puts "_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_"
+     find_wallet
+ 
+  end
+
 end
+
+## <%= inspect ahz %>
+                   
+          ##         <%= for title_huitle <- ahz do %>
+          ##         <%= title_huitle.wallet %>
+          ##         <% end %> 
